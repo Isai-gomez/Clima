aa4a7971a04fee4cffe8fec75c3db38fbeb25c4c
Chai <isaigomezhernandez.com>
2019-10-15 16:19:36 -0500

    Mostrando los resultados en el Componente

--- src/componentes/Clima.js
+++ src/componentes/Clima.js
@@ -4,7 +4,26 @@ class Clima extends Component {
   mostrarResultado = () => {
     const { name, weather, main } = this.props.resultado1;
     if (!name || !weather || !main) return null;
-    return <div className="row">{name}</div>;
+    const kelvin = 273.15;
+    const alt = `Clima de ${name}`;
+    const urlIcon = `http://openweathermap.org/img/wn/${weather[0].icon}.png`;
+    return (
+      <div className="row">
+        <div className="resultado col s12 m8 l6 offset-l3">
+          <div className="card-panel light-blue align-center">
+            <span className="white-text">
+              <h2>Resultado clima de: {name}</h2>
+              <p className="temperatura">
+                Actual: {(main.temp - kelvin).toFixed(2)} &deg;C
+                <img src={urlIcon} alt={alt} />
+              </p>
+              <p>Max. {(main.temp_max - kelvin).toFixed(2)} &deg;C</p>
+              <p>Min. {(main.temp_min - kelvin).toFixed(2)} &deg;C</p>
+            </span>
+          </div>
+        </div>
+      </div>
+    );
   };
   render() {
     return <div className="container">{this.mostrarResultado()}</div>;
